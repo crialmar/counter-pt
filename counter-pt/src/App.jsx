4e08bf61827fedbfcfed2f84d5cded0fd8e5d505
crialmar <cristina.almagon@gmail.com>
2024-11-21 19:01:17 +0100

    end

---
+++ counter-pt/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const maxLimit = 10;
+
+  const handleDecrease = () => {
+    if (count > 0) {
+      setCount((prevCount) => prevCount - 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  const handleIncrease = () => {
+    if (count < maxLimit) {
+      setCount((prevCount) => prevCount + 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  return (
+    <>
+      <div className="card">
+        <h1>Contador React</h1>
+        <h1>{count}</h1>
+        {count === maxLimit && <p>Has llegado al límite</p>}
+        <button
+          className="increase"
+          onClick={handleIncrease}
+          disabled={count === maxLimit}
+        >
+          +
+        </button>
+        <button className="reset" onClick={() => setCount(0)}>
+          Reset
+        </button>
+        <button className="decrease" onClick={handleDecrease}>
+          {console.log(count)}-
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default App;
